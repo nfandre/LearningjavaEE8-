73149ca14fa97d09f95471cb7caf5d10c92eb0f2
nfandre <andrelive.05@hotmail.com>
2021-03-08 16:46:05 -0300

    JSONP ansd JSONB

---
+++ src/main/java/com/example/JavaEE8GetStarter/json/Person.java
@@ -0,0 +1,45 @@
+package com.example.JavaEE8GetStarter.json;
+
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.Date;
+import java.util.List;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private List<String> emails;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<String> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<String> emails) {
+        this.emails = emails;
+    }
+}
---
+++ src/main/java/com/example/JavaEE8GetStarter/json/PersonWriter.java
@@ -0,0 +1,54 @@
+package com.example.JavaEE8GetStarter.json;
+
+import javax.json.*;
+import javax.json.stream.JsonGenerator;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PersonWriter {
+    private Person person;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+
+    public PersonWriter(Person person) {
+        this.person = person;
+    }
+
+    public String write() throws IOException {
+        JsonObjectBuilder jsonObjectBuilder = Json
+                .createObjectBuilder()
+                .add("firstName", person.getFirstName())
+                .add("lastName", person.getLastName())
+                .add("birthDate",  dateFormat.format(person.getBirthDate()));
+
+        final JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        person.getEmails().forEach(arrayBuilder :: add);
+        jsonObjectBuilder.add("emails", arrayBuilder);
+
+        JsonObject jsonObject = jsonObjectBuilder.build();
+
+        JsonWriterFactory writerFactory = createWriterFactory();
+        return this.writerToString(jsonObject, writerFactory);
+    }
+
+    private String writerToString(JsonObject jsonObject, JsonWriterFactory writerFactory) throws IOException {
+        String jsonString;
+        try(Writer writer = new StringWriter()){
+            writerFactory
+                    .createWriter(writer)
+                    .write(jsonObject);
+            jsonString = writer.toString();
+        }
+        return  jsonString;
+    }
+
+
+    private JsonWriterFactory createWriterFactory() {
+        Map<String,Boolean> config = new HashMap<>();
+        config.put(JsonGenerator.PRETTY_PRINTING, true);
+        return Json.createWriterFactory(config);
+    }
+}
